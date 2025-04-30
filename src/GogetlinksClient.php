bd4174549b5d41ee09a7837caecbab3da97d49d7
Maxim <amxm@ya.ru>
2025-04-30 09:58:15 +0300

    начало работы над submitArticle

--- src/GogetlinksClient.php
+++ src/GogetlinksClient.php
@@ -221,22 +221,52 @@ class GogetlinksClient implements GogetlinksInterface
         return Parser::parseTaskInfo($html);
     }
 
+
+
     /**
-     * Заказать написание текста у gogetlinks
+     * Отправляем статью
      */
-    public function orderText()
+    public function submitArticle(int $taskId, string $articleUrl)
     {
-        /**
-         * https://gogetlinks.net/copyright/createTask/currId/21188625
-         * 
-         * description: Текст подходящий под задание.
-         * lettersCount: 2
-         * mode: manual
-         * 
-         * response:
-         * {"success":true,"data":{"button_html":"<span onmouseout=\"return nd();\" onmouseover=\"return overlib(&#039;\u0417\u0430\u0434\u0430\u043d\u0438\u0435 \u043d\u0430 \u043d\u0430\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u0435\u043a\u0441\u0442\u0430 \u043e\u0442\u043f\u0440\u0430\u0432\u043b\u0435\u043d\u043e &lt;strong&gt;03.12.2021 08:49&lt;\/strong&gt;.&lt;br\/&gt;\u0421\u0442\u0430\u0442\u0443\u0441: &lt;strong&gt;\u041d\u0430\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u0435\u043a\u0441\u0442\u0430&lt;\/strong&gt;&#039;, CAPTION, &#039;\u041d\u0430\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u0435\u043a\u0441\u0442\u0430&#039;);\" class=\"icon-round-check-invert icon_small icon_green copyright-status-icon copyright-id-73443\"><\/span>"}}
-         */
+
+        // загружаем review
+        // https://gogetlinks.net/viewTask/index/curr_id/24179571/user_id/15590/type/4/task_type/review
+
+
+        // тут форма где надо указать ссылку
+        // https://gogetlinks.net/webTaskAction/beforeCheck/modal/true/curr_id/24179571/task_type/4
+
+
+        // отправка на пред проверку
+        // curr_id: 24179571
+        // URL: https://yamuzykant.ru/body-language/sekrety-zdorovojj-i-siyayuschejj-kozhi-kak-vybrat-pravilnyjj-uhod.html
+        // admin_index_ignore: 0
+        // acci: 0
+        // aicc: 0
+        // dblscid: 0
+        // task_type: 4
+        // https://gogetlinks.net/template/check_exist_view.php
+
     }
+
+
+
+    // /**
+    //  * Заказать написание текста у gogetlinks
+    //  */
+    // public function orderText()
+    // {
+    //     /**
+    //      * https://gogetlinks.net/copyright/createTask/currId/21188625
+    //      * 
+    //      * description: Текст подходящий под задание.
+    //      * lettersCount: 2
+    //      * mode: manual
+    //      * 
+    //      * response:
+    //      * {"success":true,"data":{"button_html":"<span onmouseout=\"return nd();\" onmouseover=\"return overlib(&#039;\u0417\u0430\u0434\u0430\u043d\u0438\u0435 \u043d\u0430 \u043d\u0430\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u0435\u043a\u0441\u0442\u0430 \u043e\u0442\u043f\u0440\u0430\u0432\u043b\u0435\u043d\u043e &lt;strong&gt;03.12.2021 08:49&lt;\/strong&gt;.&lt;br\/&gt;\u0421\u0442\u0430\u0442\u0443\u0441: &lt;strong&gt;\u041d\u0430\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u0435\u043a\u0441\u0442\u0430&lt;\/strong&gt;&#039;, CAPTION, &#039;\u041d\u0430\u043f\u0438\u0441\u0430\u043d\u0438\u0435 \u0442\u0435\u043a\u0441\u0442\u0430&#039;);\" class=\"icon-round-check-invert icon_small icon_green copyright-status-icon copyright-id-73443\"><\/span>"}}
+    //      */
+    // }
 
     public function getBalance()
     {
